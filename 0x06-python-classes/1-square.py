14169e91263143370668b1196097faaddcce1bf5
flipblip <wickynjy@gmail.com>
2023-06-27 10:45:27 -0700

    update files

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
-class Square:
+"""Square class"""
 
+class Square:
+"""Constructor method for square"""
     def __init__(self, size):
+        """Declare size attribute"""
         self.__size = size
--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
+"""Square class based on 1-square.py
+"""
 
 class Square:
+    """Square class with a private atr size
+    """
     
     def __init__(self, size=0):
+        """Initialize class with a private attr,size
+        """
+
         if type(size) != int:
             raise TypeError("size must be an integer")
 
