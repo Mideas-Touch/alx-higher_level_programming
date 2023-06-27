415027d782cde6f401928aec09181024de51622f
flipblip <wickynjy@gmail.com>
2023-06-27 10:57:57 -0700

    update files

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """Square class based on 1-square.py
+
 """
 
 class Square:
-    """Square class with a private atr size
+    """Square class with a private atr -size
+
     """
     
     def __init__(self, size=0):
         """Initialize class with a private attr,size
+
         """
 
         if type(size) != int:
@@ -16,4 +19,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
 
+        """Declare size variable as a private attribute
+
+        """
         self.__size = size
--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
+""""A class Square defining a square base on 1-square.py
+
+"""
 
 class Square:
+    """Square class with a private attr -size
+
+    """
     def __init__(self,size=0):
+        """Initializes the size variable as a private instance attr
+
+        """
         if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
@@ -10,4 +19,7 @@ class Square:
             self.__size = size
 
     def area(self)
+    """Returns the square area
+
+    """
         return (self.__size * self.__size)
